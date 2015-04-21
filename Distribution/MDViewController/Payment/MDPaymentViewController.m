c3cf46b6f04fb1a162bf262b266fc4a9615292a3
lsr90518 <lb90518@gmail.com>
2015-04-21 16:13:38 +0900

    peymentViewController

---
+++ Distribution/MDViewController/Payment/MDPaymentViewController.m
@@ -0,0 +1,49 @@
+//
+//  MDPaymentViewController.m
+//  Distribution
+//
+//  Created by Lsr on 4/21/15.
+//  Copyright (c) 2015 Lsr. All rights reserved.
+//
+
+#import "MDPaymentViewController.h"
+
+@interface MDPaymentViewController ()
+
+@end
+
+@implementation MDPaymentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
